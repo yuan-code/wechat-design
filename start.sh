eaa6e4891f843ffdd5842c219cb66f07d9052c17
yuanchong <yuanchong@hualala.com>
2019-07-21 19:43:39 +0800

    增加启动脚本

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /usr/local/src/wechat-design
 mvn clean install -DskipTests
-var pid = `ps -ef | grep wechat-design | grep -v grep | awk {'print $2'}`
+pid = `ps -ef | grep wechat-design | grep -v grep | awk {'print $2'}`
 kill -9 $pid
 echo 'kill pid=$pid'
 nohup java -jar -Dspring.profiles.active=www /usr/local/src/wechat-design/target/wechat-design-0.0.1-SNAPSHOT.jar
