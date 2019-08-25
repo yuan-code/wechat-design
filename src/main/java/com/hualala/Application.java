72df42ac4aaf930923f05d08322d0cf346e35ccb
yuanchong <yuanchong@hualala.com>
2019-08-25 09:14:55 +0800

    静态资源拦截的问题

--- src/main/java/com/hualala/Application.java
+++ src/main/java/com/hualala/Application.java
@@ -1,14 +1,11 @@
 package com.hualala;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
