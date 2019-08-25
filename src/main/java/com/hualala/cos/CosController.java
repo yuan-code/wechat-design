7029aa32664ca8a2082be1038a52562cee29acdd
yuanchong <yuanchong@hualala.com>
2019-08-25 15:14:46 +0800

    客户信息

--- src/main/java/com/hualala/cos/CosController.java
+++ src/main/java/com/hualala/cos/CosController.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
@@ -29,6 +30,7 @@ public class CosController {
      * @return 完整的图片路径
      * @throws IOException
      */
+    @ResponseBody
     @RequestMapping("/upload")
     public Object upload(String mediaID) throws IOException {
         if(StringUtils.isEmpty(mediaID)) {
