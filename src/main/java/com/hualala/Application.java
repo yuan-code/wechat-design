a51493fd56358a9d57382b24cde97840e4dfd42d
yuanchong <yuanchong@hualala.com>
2019-08-27 12:45:35 +0800

    支付发邮件

--- src/main/java/com/hualala/Application.java
+++ src/main/java/com/hualala/Application.java
@@ -9,15 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
-import java.util.UUID;
 
 @Controller
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println(UUID.randomUUID().toString().replaceAll("-","").toUpperCase().substring(16));
-//        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 
